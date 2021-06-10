59fee7512c0d4327a1576ce634d8605e67c43472
Addo Bernard Tawiah <bernardaddo1@gmail.com>
2021-06-10 14:10:54 +0000

    Update to contributors

--- src/components/contributors.tsx
+++ src/components/contributors.tsx
@@ -23,6 +23,11 @@ const contributors: GitHubProfileProps[] = [
         userName: 'esmond-adjei',
         role: 'Front-End Engineer',
     },
+    {
+        userName: 'BernardAddo',
+        role: 'Back-End Engineer',
+    }
+    ,
 ];
 
 export const GitHubProfile = (props: GitHubProfileProps) => {
