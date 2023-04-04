82c71915b22c3d66d222b05a100e1ad5fa635987
Christian G <43790095+ChrisGy@users.noreply.github.com>
2023-04-04 15:36:22 +0200

    Add Join us cards and make responsive

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,20 @@
 var _a;
 /* Navigation*/
 // Collapse the navbar by adding the top-nav-collapse class
+
+// On DOM Loaded:
+addEventListener("DOMContentLoaded", (event) => {
+    switch_join_us("none", 'mentees');
+});
+
+
 window.onscroll = function () {
     scrollFunction();
     scrollFunctionBTT(); // back to top button
 };
 window.onload = function () {
     scrollFunction();
+    
 };
 function scrollFunction() {
     var navbar = document.getElementById("navbarExample");
@@ -30,9 +38,9 @@ for (var i = 0; i < elements.length; i++) {
 (_a = document.querySelector(".navbar-toggler")) === null || _a === void 0
     ? void 0
     : _a.addEventListener("click", function () {
-          var _a;
-          (_a = document.querySelector(".offcanvas-collapse")) === null || _a === void 0 ? void 0 : _a.classList.toggle("open");
-      });
+        var _a;
+        (_a = document.querySelector(".offcanvas-collapse")) === null || _a === void 0 ? void 0 : _a.classList.toggle("open");
+    });
 /* Back To Top Button */
 // Get the button
 var myButton = document.getElementById("myBtn");
@@ -48,8 +56,31 @@ function scrollFunctionBTT() {
 }
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-    window.scrollTo({top: 0, behavior: 'smooth'});
+    window.scrollTo({ top: 0, behavior: 'smooth' });
     return;
     // document.body.scrollTop = 0; // for Safari
     // document.documentElement.scrollTop = 0; // for Chrome, Firefox, IE and Opera
+}
+
+// switch the "Join Us" page btn Mentees and Mentors
+function switch_join_us(pageElement, cardType) {
+    // handle paginator
+    if (pageElement != "none") {
+        document.querySelectorAll("section#join-us li.page-item").forEach((elem) => {
+            elem.classList.remove('active');
+        });
+        pageElement.classList.add('active');
+    }
+    // handle card row
+    document.querySelectorAll("section#join-us  [id^='join-us-']").forEach((join_us_row) => {
+        join_us_row.style.display = 'none';
+    });
+
+
+    if (cardType == 'mentees') {
+        document.getElementById("join-us-" + cardType + "-div").style.display = 'flex';
+    }
+    else if (cardType == 'mentors') {
+        document.getElementById("join-us-" + cardType + "-div").style.display = 'flex';
+    }
 }
